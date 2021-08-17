7f188cb3acad81560f54546cfa181def3273ab02
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-08-17 15:24:10 +0200

    add title, image and slogan

--- front/src/components/Menu/index.js
+++ front/src/components/Menu/index.js
@@ -6,6 +6,10 @@ import './styles.scss';
 export default function Menu() {
   return (
     <main className='Menu'>
+      <div className='Menu__picture'>
+        <h1 className='Menu__picture__title'>sport finder</h1>
+      </div>
+      <p className='Menu__slogan'></p>
     </main>
   );
 }
