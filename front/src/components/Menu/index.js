72016cc065b2fe7c725f739cefbff2fe0fb5a608
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-08-17 15:23:05 +0200

    creation of the menu component

---
+++ front/src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+export default function Menu() {
+  return (
+    <main className='Menu'>
+    </main>
+  );
+}
+
+// Menu.propTypes = {
+
+// };
