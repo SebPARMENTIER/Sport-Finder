cc38c5a0f4991f173a5b5c2727e20b30ecbb0a3f
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-08-18 21:31:18 +0200

    responsive header footer

---
+++ front/src/components/About/index.js
@@ -0,0 +1,30 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './about.scss';
+
+// == Component
+const About = () => (
+  <div className="about">
+    <h1 className="about__title">Présentation de l'équipe</h1>
+    <div className="about__role">
+      <div className="about__role__name">Anthony</div>
+      <div className="about__role__desc">Lead dev Front - Référent technique API Opencagedata</div>
+    </div>
+    <div className="about__role">
+      <div className="about__role__name">Christopher</div>
+      <div className="about__role__desc">Lead dev Back - Référent technique API Opencagedata</div>
+    </div>
+    <div className="about__role">
+      <div className="about__role__name">Giovanni</div>
+      <div className="about__role__desc">Git Master - Référent technique API RNA</div>
+    </div>
+    <div className="about__role">
+      <div className="about__role__name">Sébastien</div>
+      <div className="about__role__desc">Product Owner - Scrum Master</div>
+    </div>
+  </div>
+)
+
+export default About;
--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -5,6 +5,7 @@ import Footer from 'src/components/Footer';
 import Home from 'src/components/Home';
 import Profil from 'src/components/Profil';
 import Contact from 'src/components/Contact';
+import About from 'src/components/About';
 
 // == Import
 import './app.scss';
@@ -14,7 +15,8 @@ import './app.scss';
 const App = () => (
   <div className="app">
     <Header />
-    <Contact />
+    <About />
+    {/* <Contact /> */}
     {/* <Home /> */}
     {/* <Profil /> */}
     <Footer />
