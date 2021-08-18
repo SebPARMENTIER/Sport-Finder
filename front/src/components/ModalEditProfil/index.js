b001a556d906adf09d1937b7773dba95d3fedd80
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-08-18 11:33:11 +0200

    add logo

---
+++ front/src/components/ModalEditProfil/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './modalEditProfil.scss';
+
+export default function 
+ModalEditProfil() {
+  return (
+    <container className='modalEditProfil'>
+      <h1 className='modalEditProfil__title'>Modifier votre profil</h1>
+      <form className='modalEditProfil__form'>
+        <div></div>
+        <p className='modalEditProfil__form__text'>Modifier votre email</p>
+        
+      </form>
+    </container>
+  );
+}
+
+// ModalEditProfil.propTypes = {
+
+// };
--- front/src/components/Profil/index.js
+++ front/src/components/Profil/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import ModalEditProfil from 'src/components/ModalEditProfil';
 
-// import './profil.scss';
+import './profil.scss';
 
 export default function Profil() {
   return (
     <main className='profil'>
       <h1 className='profil__title'>Information du profil</h1>
       <h2 className='profil__name'>Totodu52</h2>
-      
+      <div className='profil__button'> 
+        <input className='profil__button__edit' type="button" value="Modifier votre profil" />
+        <input className='profil__button__edit' type="button" value="Modifier votre mot de passe" />
+        <input className='profil__button__edit' type="button" value="vie privée et données" />
+      </div>
+      <input className='profil__logOut' type="button" value="Déconnexion" />
+      <ModalEditProfil />
+
     </main>
   );
 }
