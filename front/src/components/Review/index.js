21d40c067ee39f7c2ea7de25babeeb7febca956c
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-08-31 17:46:45 +0200

    Add react-moment for review's date

--- front/src/components/Review/index.js
+++ front/src/components/Review/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ImPencil2, ImBin } from 'react-icons/im';
 import { FaStar } from 'react-icons/fa';
+import Moment from 'react-moment';
+import 'moment-timezone';
+
+// Set the locale for every react-moment instance to French.
+Moment.globalLocale = 'fr';
 
 // == Import : local
 import './review.scss';
@@ -37,8 +42,10 @@ const Review = ({
     <div className="review">
       <div className="review__block1">
         <div className="review__block1__header">
-        <div className="review__block1__header__from">Avis de {author.pseudo}</div>
-          <div className="review__block1__header__date">{createdAt}</div>
+          <div className="review__block1__header__from">Avis de {author.pseudo}</div>
+          <div className="review__block1__header__date">
+            <Moment fromNow>{createdAt}</Moment>
+          </div>
         </div>
         <div className="review__block1__rating">
           {/* {getStarsForOneReview()} */}
