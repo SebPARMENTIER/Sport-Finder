c7cd8bc484f7dce5b47137b97ec54b54f2fb23b2
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-09-01 15:52:41 +0200

    Add logged props on App component to access on profil route

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,38 +1,42 @@
 // == Import npm
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Header from 'src/containers/Header';
 import Footer from 'src/containers/Footer';
 import Home from 'src/containers/Home';
 import Profil from 'src/containers/Profil';
 import Contact from 'src/containers/Contact';
 import About from 'src/containers/About';
-import ModalPrivacyData from 'src/containers/ModalPrivacyData';
-import ModalEditProfil from 'src/components/ModalEditProfil';
-import ModalAddReview from 'src/components/ModalAddReview';
 import Single from 'src/containers/Single';
 import Results from 'src/containers/Results';
+import Error from 'src/components/Error';
 
 import './app.scss';
 
-const App = () => (
+const App = ({
+  logged,
+}) => (
   <div className="app">
     <Header />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/about" component={About} />
-      <Route exact path="/profil" component={Profil} />
-      <Route exact path="/privacydata" component={ModalPrivacyData} />
-      <Route exact path="/editprofil" component={ModalEditProfil} />
-      <Route exact path="/addreview" component={ModalAddReview} />
+      {logged && (
+        <Route exact path="/profil" component={Profil} />
+      )}
       <Route exact path="/single/:id" component={Single} />
       <Route exact path="/results" component={Results} />
+      <Route component={Error} />
     </Switch>
-    {/* <ModalAddReview /> */}
     <Footer />
   </div>
 );
+
+App.propTypes = {
+  logged: PropTypes.bool.isRequired,
+};
 
 // == Export
 export default App;
