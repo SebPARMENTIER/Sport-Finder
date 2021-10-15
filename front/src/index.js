c6562c0b4c3f1a414d9410122e8bdf29e35d18dd
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-10-15 09:24:00 +0200

    Add comment on index.js

--- front/src/index.js
+++ front/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
@@ -6,13 +7,9 @@ import { Provider } from 'react-redux';
 
 // == Import : local
 import store from 'src/store';
-
-// Composants
 import App from 'src/containers/App';
 
-// == Render
-// 1. Élément React racine (celui qui contient l'ensemble de l'app)
-//    => crée une structure d'objets imbriqués (DOM virtuel)
+// == Component
 const rootReactElement = (
   <Provider store={store}>
     <BrowserRouter>
@@ -20,7 +17,7 @@ const rootReactElement = (
     </BrowserRouter>
   </Provider>
 );
-// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+
 const target = document.getElementById('root');
-// 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
+
 render(rootReactElement, target);
