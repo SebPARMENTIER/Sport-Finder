ac7472931123becb6ca986a111776934e2610c6e
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-10-15 09:24:51 +0200

    Add comment on index.js

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,7 @@
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 // == Import : local
